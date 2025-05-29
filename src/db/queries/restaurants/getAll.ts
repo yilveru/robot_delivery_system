f11f670c1ad55bce7f1d981cce4a0151e42d710e
unknown <yilant85@gmail.com>
2025-05-29 11:28:24 -0500

    change the order of the clients and restaurants

--- src/db/queries/restaurants/getAll.ts
+++ src/db/queries/restaurants/getAll.ts
@@ -1,9 +1,11 @@
 import { db } from "@/db";
 import { restaurants } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
 export async function getAll() {
 
     return await db
         .select()
-        .from(restaurants);
+        .from(restaurants)
+        .orderBy(desc(restaurants.name));
 }
