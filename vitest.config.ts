324e7226c2cf15d4752dd63e445180e09b06ccf9
unknown <yilant85@gmail.com>
2025-06-03 00:48:38 -0500

    change import package to fix the deploy

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
